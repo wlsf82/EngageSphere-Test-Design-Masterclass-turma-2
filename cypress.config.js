9c8d90b6ff82a9e737ae437e09b390030ad0267d
ldasilvaNZ11 <lucas.cordeiro@gmail.com>
2024-07-09 14:35:42 +1200

    pointing my config file to the correct port

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,16 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: 'http://localhost:3001',
+    env: {
+      hideCredentials: true,
+      requestMode: true,
     },
   },
-};
+  fixturesFolder: false,
+  video: false,
+  setupNodeEvents(on, config) {
+    // implement node event listeners here
+  }, 
+})
--- cypress/support/api_commands.js
+++ cypress/support/api_commands.js
@@ -1,7 +1,7 @@
 
 Cypress.Commands.add('api_retrieveCustomers', (page=1, limit=10, size='All') => {
-    cy.api({
+    cy.request({
         method: 'GET',
-        url: `http://localhost:3000/customers?$page=${page}&limit=${limit}&size=${size}`,
+        url: `customers?$page=${page}&limit=${limit}&size=${size}`,
     })
   })
