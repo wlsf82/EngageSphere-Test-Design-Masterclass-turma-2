c847e43ef154bcae148e4f04f13dc1c06909d56b
ldasilvaNZ11 <lucas.cordeiro@gmail.com>
2024-07-15 10:38:51 +1200

    Addressing PR comments

--- cypress.config.js
+++ cypress.config.js
@@ -10,7 +10,6 @@ module.exports = defineConfig({
     },
   },
   fixturesFolder: false,
-  video: false,
   setupNodeEvents(on, config) {
     // implement node event listeners here
   }, 
--- cypress/support/api_commands.js
+++ cypress/support/api_commands.js
@@ -2,6 +2,6 @@
 Cypress.Commands.add('api_retrieveCustomers', (page = 1, limit = 10, size = 'All') => {
     cy.request({
         method: 'GET',
-        url: `http://localhost:3001/customers?$page=${page}&limit=${limit}&size=${size}`
+        url: `${Cypress.env('API_URL')}/customers?$page=${page}&limit=${limit}&size=${size}`
     })
 })
