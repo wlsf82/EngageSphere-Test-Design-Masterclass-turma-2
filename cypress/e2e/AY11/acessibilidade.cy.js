30a9b1a9e0bdbdeeb2f4f7de57e1274303a0292d
Guilherme Santana <guilhermerootz@gmail.com>
2024-07-22 16:48:02 -0300

    Incluindo testes de acessibilidade no modo dark e light v2

--- cypress/e2e/AY11/acessibilidade.cy.js
+++ cypress/e2e/AY11/acessibilidade.cy.js
@@ -1,47 +1,51 @@
 /// <reference types="cypress" />
 import 'cypress-axe'
 
-describe('TESTE DE ACESSIBILIDADE', () => {
-  beforeEach(() => {
-    cy.visit('/')
-    cy.injectAxe()
-  });
-  // Basic usage
-  it('VERIFICA A11Y E FINALIZA', () => {
-    // Test the page at initial load
-    cy.checkA11y()
+describe('TESTES DE ACESSIBILIDADE', () => {
+
+  context('VERIFICA ACESSIBLIDADE NO MODO DARK', () => {
+    beforeEach(() => {
+      cy.visit('/')
+      cy.injectAxe()
+      cy.get('#theme-toggle-button').click()
+      cy.get('[data-theme="dark"]').should('exist')
+    });
+
+    it('VERIFICA ACESSIBILIDADE NO MODO DARK', () => {
+      cy.pageAccessibility();
+    });
+
+    it('NÃO ENCONTRA ERROS DE AY11 EM DETALHES DO CLIENTE COM MODO DARK', () => {
+      cy.get('tbody').first().click()
+      cy.checkA11y()
+    });
+
+    it('NÃO ENCONTRA ERROS DE AY11 NO DETALHE DO ENDEREÇO COM MODO DARK', () => {
+      cy.get('tbody').first().click()
+      cy.get('.show-address-btn').click()
+      cy.checkA11y()
+    });
   })
 
-  it('VERIFICA ACESSIBILIDADE NO MODO DARK', () => {
-    cy.get('#theme-toggle-button').click()
-    cy.pageAccessibility();
+  context('VERIFICA ACESSIBLIDADE NO MODO LIGHT', () => {
+    beforeEach(() => {
+      cy.visit('/')
+      cy.injectAxe()
+    });
 
-  });
+    it('VERIFICA ACESSIBILIDADE NO MODO DARK', () => {
+      cy.pageAccessibility();
+    });
 
-  // // Applying a context and run parameters
-  it('VERIFICA ACESSIBILIDADE DE UMA CLASSE OU ID ESPECÍFICO', () => {
-    // Test the page at initial load (with context and options)
-    cy.checkA11y('.container', {
-      runOnly: {
-        type: 'tag',
-        values: ["wcag2a", "wcag244", "wcag412"]
-      }
-    })
+    it('NÃO ENCONTRA ERROS DE AY11 EM DETALHES DO CLIENTE COM MODO DARK', () => {
+      cy.get('tbody').first().click()
+      cy.checkA11y()
+    });
+
+    it('NÃO ENCONTRA ERROS DE AY11 NO DETALHE DO ENDEREÇO COM MODO DARK', () => {
+      cy.get('tbody').first().click()
+      cy.get('.show-address-btn').click()
+      cy.checkA11y()
+    });
   })
-
-  it('ERROS CRÍTICOS', () => {
-    // Test on initial load, only report and assert for critical impact items
-    cy.checkA11y(null, {
-      includedImpacts: ['critical']
-    })
-  })
-
-  it('FINALIZA O TESTE MESMO QUE HAJA ERROS', () => {
-    // Do not fail the test when there are accessibility failures
-    cy.checkA11y(null, null, null, true)
-  })
-
-  it('VALIDAÇÃO TOTAL DE ACESSIBILIDADE', () => {
-    cy.pageAccessibility();
-  });
 });
