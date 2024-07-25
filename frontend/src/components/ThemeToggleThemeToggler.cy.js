babf0b516e02141dfde2952682c832f39f247783
Thaynara <tdutra@redventures.com>
2024-07-25 17:11:44 -0300

    just a fix

--- frontend/src/components/ThemeToggleThemeToggler.cy.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ThemeToggler from './ThemeToggle'
-
-describe('<ThemeToggler />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<ThemeToggler />)
-  })
-})
